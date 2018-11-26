059b8c4245e47ca41c6782b7be912e2135d32e69
quan <15600777211@163.com>
2018-11-26 23:58:19 +0800

    "git2 一次"

--- src/main/java/A.java
+++ src/main/java/A.java
@@ -2,6 +2,6 @@ public class A {
     public static void main(String[] args) {
         System.out.println("git2");
         System.out.println("git 重写");
-        System.out.println("chongxin xie");
+
     }
 }
